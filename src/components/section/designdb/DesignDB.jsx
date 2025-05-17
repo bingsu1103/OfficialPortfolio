8f77621a60779333c34bb655f038bf2897adec63
An Ngo <ngogiaan11111@gmail.com>
2025-05-17 21:12:01 +0700

    commit db

--- src/components/section/designdb/DesignDB.jsx
+++ src/components/section/designdb/DesignDB.jsx
@@ -4,6 +4,10 @@ import Skill from "../../../assets/svg/SKILLDB.drawio.svg?react"
 import Experience from "../../../assets/svg/EXPERIENCEDB.drawio.svg?react"
 import Project from "../../../assets/svg/PROJECTDB.drawio.svg?react"
 import Education from "../../../assets/svg/EDUCATIONDB.drawio.svg?react"
+import UserSkill from "../../../assets/svg/USER_SKILL2.drawio.svg?react"
+import UserExperience from "../../../assets/svg/USER_EXPERIENCE.drawio.svg?react"
+import UserProject from "../../../assets/svg/USER_PROJECT.drawio.svg?react"
+import UserEducation from "../../../assets/svg/USER_EDUCATION.drawio.svg?react"
 
 const DesignDB = () => {
     return (
@@ -18,6 +22,10 @@ const DesignDB = () => {
                 <Experience width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] circle-animation h-20" />
                 <Project width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] bounce-horizontal2 h-25" />
                 <Education width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] bounce-horizontal h-20" />
+                <UserSkill width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] bounce-horizontal3 h-15" />
+                <UserExperience width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] circle-animation4 h-10" />
+                <UserProject width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] circle-animation5 h-10" />
+                <UserEducation width={100} className="block [clip-path:inset(0px_0px_2px_0px)] rounded-lg rounded-b-[11px] circle-animation6 h-10" />
             </div>
         </div>
 
